9a4d5e7796bd2805a59b525e8bff71275dd6cd38
Jacob Kagon <jacob@kagon.net>
2020-10-05 01:51:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
  def run_guessing_game
+	roll = rand(6) + 1 
 	puts "Guess a number betwen 1 and 6."
-	roll = 1+rand(6)
 	user_input = gets.chomp
     if user_input == roll.to_s 
     	puts "You guessed the correct number!"
